9398b7c21e5895b7f8de6497bd126d84ef1e4333
Kriess Dukes <>
2021-04-12 22:47:01 +0000

    I did my coding.

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ should be a string and NOT the friends array.
 function friendsOfCustomer(name){
   //CODE BELOW HERE
 
-
+ for(var i = 0; <= customers.length - 1; i++){
+  if(name === customers[i].name){
+    for(var o = 0; o <= customers{i}.friends.length - 1; o++)
+     console.log(customers[i].friends[o].name)
+   }
+  }
+ }
 
   //CODE ABOVE HERE
 }
@@ -31,8 +37,12 @@ function.
 
 function contactInfo(name){
   //CODE BELOW HERE
-
-
+ return(contactInfo.number.adress){
+   for(var i = 0; <= customer.phoneNumber - 1; i++){
+  if(name === customers[i].name){
+    for(var o = 0; o <= customer.adress - 1; o++)
+     console.log(customers[i].phoneNumber[o].adress)
+ }
 
   //CODE ABOVE HERE
 }
